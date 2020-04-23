81a78de22caa9929abf9d33011302419a61502cb
kilbot <paul@kilbot.com.au>
2020-04-23 19:00:57 +0400

    update deps

--- packages/common/src/services/auth.tsx
+++ packages/common/src/services/auth.tsx
@@ -10,6 +10,7 @@ export default class AuthService {
 		try {
 			GoogleSignin.configure({
 				// scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+				webClientId: '625487084049-uin80od6d95rpdf8ga4f1jhhch0hp5jd.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
 				// offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
 				// hostedDomain: '', // specifies a hosted domain restriction
 				// loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
